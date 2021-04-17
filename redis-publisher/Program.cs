bda30fddd7797142a271c6410487735280b585d0
Abdulkerim Karaman <abdulkerim.karamna@albarakatech.com>
2021-04-17 14:24:39 +0300

    first commit

---
+++ redis-publisher/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using StackExchange.Redis;
+
+namespace redis_publisher
+{
+    class Program
+    {
+        private const string RedisConnectionString = "localhost:6379";
+        private static ConnectionMultiplexer connection = ConnectionMultiplexer.Connect(RedisConnectionString);
+        private const string Channel = "channel_one";
+        static void Main(string[] args)
+        {
+            var pubsub = connection.GetSubscriber();
+            // Mesajı kanala gönderiyoruz.
+            pubsub.PublishAsync(Channel, "Deneme Mesaji!", CommandFlags.FireAndForget);
+            Console.Write("Mesaj gönderildi.");
+        }
+    }
+}
---
+++ redis-subscriber/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using StackExchange.Redis;
+
+namespace redis_subscriber
+{
+    class Program
+    {
+        private const string RedisConnectionString = "localhost:6379";
+        private static ConnectionMultiplexer connection = ConnectionMultiplexer.Connect(RedisConnectionString);
+        private const string Channel = "channel_one";
+        static void Main(string[] args)
+        {
+            // Create pub/sub
+            var pubsub = connection.GetSubscriber();
+
+            // Subscriber subscribes to a channel
+            pubsub.Subscribe(Channel, (channel, message) => Console.Write(message));
+            Console.ReadLine();
+        }
+    }
+}
